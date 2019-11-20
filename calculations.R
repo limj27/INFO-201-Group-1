72820df78629054b5895b1ce3bdffa54901b841e
hannanajmal <hajmal05@uw.edu>
2019-11-20 15:46:07 -0800

    chnages made

--- calculations.R
+++ calculations.R
@@ -1,19 +1,31 @@
 library("readxl")
 library("ggplot2")
+library("dplyr")
 
-stats_df <- read.csv("data/2012-18_teamBoxScore.csv", stringsAsFactors = FALSE)
-attendance_df <- read_excel("data/NBA Team Annual Attendance.xlsx")
+stats_df <- read_xlsx("data/nba_team_stats.xlsx")
+attendance_df <- read_xlsx("data/NBA Team Annual Attendance.xlsx")
+wins_df <- read.xlsx("data/")
 
 View(stats_df)
 View(attendance_df)
 
 # Merging Dataframes
-attendance <- attendance_df %>% 
-  select(`Starting Year`, Team, `Home: Avg Attendance`)
+attendance <- attendance_df %>%
+  select(`Start Year`, Team, `Home: Avg Attendance`) %>%
+  View()
 
-stats <- stats_df %>% 
-  select(gmDate, teamAbbr, teamRslt)
+wins <- wins_df %>% 
+  select(`Start Year`, Team, `Win Percentage`)
 
+wins_percentages <- wins %>% 
+  group_by(Team) %>% 
+  
+  # Teams stats vs fan attendance 
+  stats <- stats_df %>%
+  select(`Start Year`, Team,`FG%`, `3P%`, `FT%`, TOV, RPG, APG, PPG) %>%
+  View()
+df_new <- full_join(attendance_df, stats_df, by = c("Start Year"))
+View(df_new)
 
 
 # Plot
