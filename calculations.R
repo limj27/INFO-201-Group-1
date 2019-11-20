222286aeb367ebcbb96765a54626c34053488614
hannanajmal <hajmal05@uw.edu>
2019-11-20 15:11:14 -0800

    changes made- Hannan

--- calculations.R
+++ calculations.R
@@ -6,15 +6,27 @@ stats_df <- read_xlsx("data/nba_team_stats.xlsx")
 attendance_df <- read_xlsx("data/NBA Team Annual Attendance.xlsx")
 wins_df <- read.xlsx("data/")
 
+View(stats_df)
+View(attendance_df)
+
 # Merging Dataframes
 attendance <- attendance_df %>%
-  select(`Starting Year`, Team, `Home: Avg Attendance`)
+  select(`Starting Year`, Team, `Home: Avg Attendance`) %>%
+View()
 
 wins <- wins_df %>% 
   select(`Start Year`, Team, `Win Percentage`)
 
 wins_percentages <- wins %>% 
   group_by(Team) %>% 
+  
+# Teams stats vs fan attendance 
+stats <- stats_df %>%
+  select(`Start Year`, `FG%`, `3P%`, `FT%`, TOV, RPG, APG, PPG) %>%
+  View()
+
+df_new <- left_join(attendance_df, stats_df, by = c("Start Year", "Team"))
+View(df_new)
   
   
 
