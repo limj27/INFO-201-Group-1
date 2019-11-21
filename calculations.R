0f56be6e6a96741024f821710568a6005bff8de9
jordanjones243 <jojo2018@live.com>
2019-11-20 17:08:28 -0800

    Changes to Master in Calculations

--- calculations.R
+++ calculations.R
@@ -1,30 +1,56 @@
 library("readxl")
 library("ggplot2")
 library("dplyr")
+library("plotly")
 
-stats_df <- read_xlsx("data/nba_team_stats.xlsx")
-attendance_df <- read_xlsx("data/NBA Team Annual Attendance.xlsx")
-wins_df <- read.xlsx("data/")
+stats_df <- read_excel("data/nba_team_stats.xlsx")
+attendance_df <- read_excel("data/NBA Team Annual Attendance.xlsx")
+wins_df <- read_excel("data/team_win_percent.xlsx")
 
-# Merging Dataframes
+colnames(wins_df) <- c("Start Year", "Team", "Win Percentage", "Last 3 Games", "Last Game", "Home", 
+                       "Away", "Previous Year Win Percentage")
+View(wins_df)
+
+
+
+# -------------- Merging Dataframes -----------------
+
+colnames(attendance_df)[1] <- "Start Year"
+
+# Getting columns of interest
 attendance <- attendance_df %>%
-  select(`Starting Year`, Team, `Home: Avg Attendance`)
+  select(`Start Year`, Team, `Home: Avg Attendance`)
+
+# Getting attendance for each team
+attendance_per_team <- attendance %>% 
+  group_by(`Start Year`, Team) %>%
+print(attendance_per_team)
 
 wins <- wins_df %>% 
   select(`Start Year`, Team, `Win Percentage`)
 
 wins_percentages <- wins %>% 
-  group_by(Team) %>% 
-  
-  
+  group_by(`Start Year`, Team) %>% 
+  pull(`Win Percentage`)
 
-
+wins_and_attend <- left_join(wins, attendance, by = c("Start Year", "Team"))
 
 # Plot
-plot <- ggplot(data = stats_df) +
-  geom_line(mapping = aes(x = , y =)) +
+plot <- ggplot(data = wins_and_attend) +
+  geom_line(mapping = aes(x = `Win Percentage`, y = `Home: Avg Attendance`, group = `Start Year`)) +
   labs(
-    title = "NBA Team Stats vs. Fan Attendance",
-    x = "Team Stats by Season",
+    title = "NBA Team Wins vs. Fan Attendance",
+    x = "Team Wins by Season",
     y = "Fan Attendance"
   )
+
+int_plot <- plot_ly(
+  data = wins_and_attend,
+  x = ~`Win Percentage`,
+  y = ~`Home: Avg Attendance`,
+  color = ~c(Team, `Start Year`),
+  type = 
+)
+
+p_plot <- ggplotly(plot)
+
